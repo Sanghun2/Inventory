c11720ad42a8aa9da1a98cea9a693f242d66c3b5
lshzang86 <lshzang86@naver.com>
2026-03-30 01:00:24 +0900

    inventory tag 추가

--- Runtime/InventoryBase.cs
+++ Runtime/InventoryBase.cs
@@ -8,20 +8,23 @@ namespace BilliotGames
         public int Capacity => capacity;
         public string InventoryID => inventoryID;
         public int SearchPriority => searchPriority;
+        public string Tag => tag;
 
         [SerializeField] protected int capacity;
         [SerializeField] protected string inventoryID;
         [SerializeField] protected int searchPriority;
+        [SerializeField] protected string tag;
         protected bool isInit;
 
         public virtual event Action<ItemStack, int> OnItemAdded;
         public virtual event Action<ItemStack, int> OnItemMerged;
         public virtual event Action<ItemStack, int> OnItemRemoved;
 
-        public InventoryBase(string id, int capacitiy, int searchPriority=5) {
+        public InventoryBase(string id, int capacitiy, string tag="none", int searchPriority=5) {
             this.inventoryID = id;
             this.capacity = capacitiy;
             this.searchPriority = searchPriority;
+            this.tag = tag;
         }
 
         public void SetSearchPriority(int searchPriority) {
