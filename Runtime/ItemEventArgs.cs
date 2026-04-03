687ea79d1998aca15b0157cb48615555f1fceb54
lshzang86 <lshzang86@naver.com>
2026-04-03 13:40:23 +0900

    item event args weight 필드 추가

--- Runtime/ItemEventArgs.cs
+++ Runtime/ItemEventArgs.cs
@@ -5,10 +5,20 @@ public readonly struct ItemEventArgs
     public readonly string itemID;
     public readonly int delta;
     public readonly int index;
+    public readonly int weight;
 
-    public ItemEventArgs(string itemID, int delta, int index=-1) {
+    public ItemEventArgs(string itemID, int delta) {
         this.itemID = itemID; 
         this.delta = delta; 
+        this.index = -1;
+        this.weight = 0;
+    }
+
+    public ItemEventArgs(string itemID, int delta, int index) : this(itemID, delta) {
         this.index = index;
+    }
+
+    public ItemEventArgs(string itemID, int delta, int index, int weight) : this (itemID, delta, index) {
+        this.weight = weight;
     }
 }
