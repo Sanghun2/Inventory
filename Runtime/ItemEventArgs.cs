daafd49e5bad24ae077120757724350c02b37a35
lshzang86 <lshzang86@naver.com>
2026-04-03 13:48:52 +0900

    item event args namespace 래핑

--- Runtime/ItemEventArgs.cs
+++ Runtime/ItemEventArgs.cs
@@ -1,24 +1,27 @@
 ﻿using UnityEngine;
 
-public readonly struct ItemEventArgs
+namespace BilliotGames
 {
-    public readonly string itemID;
-    public readonly int delta;
-    public readonly int index;
-    public readonly int weight;
+    public readonly struct ItemEventArgs
+    {
+        public readonly string itemID;
+        public readonly int delta;
+        public readonly int index;
+        public readonly int weight;
 
-    public ItemEventArgs(string itemID, int delta) {
-        this.itemID = itemID; 
-        this.delta = delta; 
-        this.index = -1;
-        this.weight = 0;
-    }
+        public ItemEventArgs(string itemID, int delta) {
+            this.itemID = itemID;
+            this.delta = delta;
+            this.index = -1;
+            this.weight = 0;
+        }
 
-    public ItemEventArgs(string itemID, int delta, int index) : this(itemID, delta) {
-        this.index = index;
-    }
+        public ItemEventArgs(string itemID, int delta, int index) : this(itemID, delta) {
+            this.index = index;
+        }
 
-    public ItemEventArgs(string itemID, int delta, int index, int weight) : this (itemID, delta, index) {
-        this.weight = weight;
+        public ItemEventArgs(string itemID, int delta, int index, int weight) : this(itemID, delta, index) {
+            this.weight = weight;
+        }
     }
 }
